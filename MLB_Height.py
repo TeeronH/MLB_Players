ae366065957ac0a5e3878fca57007c40adf6943e
Teeron Hajebi <teeron@teerons-air.lan>
2020-09-19 08:41:05 -0700

    Finding the 75th percentile of height.

--- MLB_Height.py
+++ MLB_Height.py
@@ -1,10 +1,24 @@
 # Find the 75th percentile of MLB players heights
 
 import csv
+import statistics
+import collections
 
 height = []
 with open ('MLB_Players.csv') as csvfile:
 	reader = csv.reader(csvfile)
+	next(reader)
 	for row in reader:
-		height.append(row[0])
-print(height)
+		height.append(int(row[3]))
+
+	height_percentiles = statistics.quantiles(height, n = 4, method = 'exclusive')
+	print(height_percentiles[-1])
+
+	d = collections.Counter(height)
+	print(d)
+
+	sorted_keys = sorted(d)
+	for key in sorted_keys:
+		value = d[key]
+		string = '*' * value
+		print(str(key) + ':' + string)
